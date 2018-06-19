f51bb92803fa7aafd5a0fb9a909846b50c874e02
Artur Dąbrowski <artur@lifeinmobile.com>
2018-06-19 12:51:44 +0200

    before eject

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
+import Radium, {StyleRoot} from 'radium';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -51,34 +52,78 @@ class App extends Component {
       });
     }
   };
+    switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        {name: newName, age: 29},
+        {name: 'Dab', age: 29},
+        {name: 'Dab', age: 29}
+      ]
+    })
+  }
 
 
   render() {
+    const style = {
+      backgroundColor: 'green',
+      padding: '8px',
+      curson: 'pointer',
+      border: 'none',
+      color: 'white',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
+    }
+
+    const assignedClasses = [];
+    if(this.state.persons.length <= 2){
+      assignedClasses.push(classes.red);
+    }
+    if (this.state.persons.length <= 1) {
+      assignedClasses.push(classes.bold);
+    }
+
     let persons = null;
     if(this.state.showPersons){
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person 
+            return (
+            <Person 
                       name={person.name}
                       age={person.age} 
                       key={person.id} 
-                      // click={() => this.deletePersonHandler(index)}
+                      click={() => this.deletePersonHandler(index)}
                       changed={(event) => this.nameChangeHandler(event, person.id)}
                       />
+            );
           })}
         </div>
       )
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
-    return <div className="App">
-        <h1>Hello, Im a React app</h1>
-        <button onClick={() => this.switchNameHandler()}>Switch name</button>
-        <button onClick={this.togglePersonHandler}>{this.state.toggleButton}</button>
-        {persons}
-      </div>;
+    return <StyleRoot>
+        <div className={classes.App}>
+          <h1 className={assignedClasses.join(" ")}>
+            Hello, Im a React app
+          </h1>
+          <button onClick={() => this.switchNameHandler()}>
+            Switch name
+          </button>
+          <button onClick={this.togglePersonHandler} style={style}>
+            {this.state.toggleButton}
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>;
   }
 }
 
-export default App;
+export default Radium(App);
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import Person from './Person.css'
+import Person from './Person.css';
+import Radium from "radium";
+
 
 const person = (props) => {
-    return <div className="Person">
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  }
+    return <div className="Person" style={style}>
         <p onClick={props.click}>
           {props.name} {props.age}{props.children}
           <input type="text" onChange={props.changed} value={props.name}/>
@@ -10,4 +17,4 @@ const person = (props) => {
       </div>;
 }
 
-export default person;
+export default Radium(person);
